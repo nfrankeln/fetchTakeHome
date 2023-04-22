2ff9e08f50159f06c0f19e3c04794b6551bf18d9
nfrankeln <ntnyfranky@gmail.com>
2023-04-22 00:08:28 -0500

    heroku production test

--- fetchFrontEnd/src/App.jsx
+++ fetchFrontEnd/src/App.jsx
@@ -4,30 +4,25 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
-
-
+import Navbar from './components/NavBar';
+import LoginPage from './pages/LoginPage';
+import SearchPage from './pages/SearchPage';
 import SearchResults from './components/SearchResults';
-import FavoritesPage, { getFavDogs } from './pages/FavoritesPage';
+import FavoritesPage, { getFavDogs } from './pages/FavoritesPage'
 import MatchPage, { getMatch } from './pages/MatchPage';
 
-import { lazy } from 'react';
 
-const SearchPage = lazy(()=>import('./pages/SearchPage'))
-const Navbar = lazy(()=> import('./components/NavBar'))
+
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Navbar />}>
+      <Route path="/" element={<Navbar/>}>
         <Route path="search" element={<SearchPage />}>
           <Route path=":breed?/:page?/:sort?" element={<SearchResults />} />
         </Route>
-        <Route
-          path="favorites"
-          element={<FavoritesPage />}
-          loader={getFavDogs}
-        />
-        <Route path="match" element={<MatchPage />} loader={getMatch}></Route>
+        <Route path = "favorites" element={<FavoritesPage/>} loader={getFavDogs} />
+        <Route path = "match" element={<MatchPage/>} loader={getMatch}></Route>
       </Route>
     )
   );
